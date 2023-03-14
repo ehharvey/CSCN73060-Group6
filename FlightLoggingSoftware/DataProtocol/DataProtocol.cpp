d59139e5391495885ea7057e7177f5a064ccd479
Jacky Lam <jlam1@conestogac.on.ca>
2023-03-14 00:24:02 +0000

    Implemented data protocol functions

--- FlightLoggingSoftware/DataProtocol/DataProtocol.cpp
+++ FlightLoggingSoftware/DataProtocol/DataProtocol.cpp
@@ -1,1 +1,45 @@
 #include "DataProtocol.h"
+#include <cstring>
+
+namespace DataProtocol {
+
+const uint_fast8_t flightIdOffset = 0;
+const uint_fast8_t secondDeltaOffset = flightIdOffset + sizeof(uint_fast64_t);
+const uint_fast8_t fuelLevelOffset = secondDeltaOffset + sizeof(uint_fast64_t);
+
+ClientTransmission::ClientTransmission(uint_fast64_t flight_id,
+                                       uint_fast64_t secondDelta,
+                                       uint_fast64_t fuelLevel) {
+
+  std::memcpy(payload.data() + flightIdOffset, &flight_id, sizeof(flight_id));
+  std::memcpy(payload.data() + secondDeltaOffset, &secondDelta,
+              sizeof(secondDelta));
+  std::memcpy(payload.data() + fuelLevelOffset, &fuelLevel, sizeof(fuelLevel));
+}
+
+ClientTransmission::ClientTransmission(std::byte *buffer) {
+  std::memcpy(payload.data(), buffer, PACKET_SIZE);
+}
+
+uint_fast64_t ClientTransmission::getFlightId() {
+  uint_fast64_t flight_id;
+  std::memcpy(&flight_id, payload.data() + flightIdOffset, sizeof(flight_id));
+  return flight_id;
+}
+
+uint_fast64_t ClientTransmission::getSecondDelta() {
+  uint_fast64_t secondDelta;
+  std::memcpy(&secondDelta, payload.data() + secondDeltaOffset,
+              sizeof(secondDelta));
+  return secondDelta;
+}
+
+uint_fast64_t ClientTransmission::getFuelLevel() {
+  uint_fast64_t fuelLevel;
+  std::memcpy(&fuelLevel, payload.data() + fuelLevelOffset, sizeof(fuelLevel));
+  return fuelLevel;
+}
+
+std::byte *ClientTransmission::getPayload() { return payload.begin(); }
+
+} // namespace DataProtocol
--- FlightLoggingSoftware/DataProtocol/DataProtocol.h
+++ FlightLoggingSoftware/DataProtocol/DataProtocol.h
@@ -4,32 +4,26 @@
 #pragma once
 
 namespace DataProtocol {
-    // 64 bits (8 bytes): Flight ID
-    // 64 bits (8 bytes): Second delta
-    // 64 bits (8 bytes): fuel level
-    // 24 bytes total
-    const uint_fast8_t PACKET_SIZE = sizeof(uint64_t) * 3;
+// 64 bits (8 bytes): Flight ID
+// 64 bits (8 bytes): Second delta
+// 64 bits (8 bytes): fuel level
+// 24 bytes total
+const uint_fast8_t PACKET_SIZE = sizeof(uint64_t) * 3;
 
-    class ClientTransmission
-    {
-        private:
-        std::array<std::byte, PACKET_SIZE> payload;
+class ClientTransmission {
+private:
+  std::array<std::byte, PACKET_SIZE> payload;
 
-        public:
-        ClientTransmission(
-            uint_fast64_t flight_id,
-            uint_fast64_t secondDelta,
-            uint_fast64_t fuelLevel
-        );
+public:
+  ClientTransmission(uint_fast64_t flight_id, uint_fast64_t secondDelta,
+                     uint_fast64_t fuelLevel);
 
-        ClientTransmission(
-            std::unique_ptr<std::byte> payload
-        );
+  ClientTransmission(std::byte* payload);
 
-        uint_fast64_t getFlightId();
-        uint_fast64_t getSecondDelta();
-        uint_fast64_t getFuelLevel();
+  uint_fast64_t getFlightId();
+  uint_fast64_t getSecondDelta();
+  uint_fast64_t getFuelLevel();
 
-        std::byte* getPayload();
-    };
-}
+  std::byte *getPayload();
+};
+} // namespace DataProtocol
