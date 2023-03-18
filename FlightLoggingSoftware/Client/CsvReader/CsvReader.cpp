bc1467e6ec4d63a97162af5fd44c8a6c99c8b43e
Jacky Lam <jlam1@conestogac.on.ca>
2023-03-18 03:45:51 +0000

    CsvReader Functions Implemented

--- FlightLoggingSoftware/Client/CsvReader/CsvReader.cpp
+++ FlightLoggingSoftware/Client/CsvReader/CsvReader.cpp
@@ -1,19 +1,53 @@
 #include "CsvReader.h"
+#include <regex>
+#include <time.h>
 
-Client::CsvReader::CsvReader(std::filesystem::path path)
-{
+namespace Client {
+
+extern uint_fast64_t flight_id;
+extern uint_fast64_t initialTime;
+
+CsvReader::CsvReader(std::filesystem::path path) {
+  try {
+    ifs = std::ifstream(path, std::ios::in);
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
 }
 
-void Client::CsvReader::start()
-{
+void CsvReader::start() {}
+
+bool CsvReader::isAtEnd() { return ifs.eof(); }
+
+std::unique_ptr<DataProtocol::ClientTransmission>
+CsvReader::getNextTransmission() {
+  std::string line;
+  struct tm tm {};
+
+  while (std::getline(ifs, line)) {
+    const char *strp_res = strptime(line.c_str(), "%d_%m_%Y %H:%M:%S", &tm);
+
+    // If didn't find the time
+    if (strp_res == nullptr)
+      continue;
+
+    uint_fast64_t secondDelta = 0;
+
+    // First time finding time
+    if (Client::initialTime == 0) {
+      Client::initialTime = std::mktime(&tm);
+    } else {
+      secondDelta = std::mktime(&tm) - Client::initialTime;
+    }
+    std::string subString(strp_res + 1);
+    // To get fuel as Int, remove "."
+    uint_fast64_t fuelLevel =
+        std::stoi(subString.erase(subString.find("."), 1));
+
+    return std::unique_ptr<DataProtocol::ClientTransmission>(
+        new DataProtocol::ClientTransmission(Client::flight_id, secondDelta,
+                                             fuelLevel));
+  }
 }
 
-bool Client::CsvReader::isAtEnd()
-{
-    return true;
-}
-
-std::unique_ptr<DataProtocol::ClientTransmission> Client::CsvReader::getNextTransmission()
-{
-    return std::unique_ptr<DataProtocol::ClientTransmission>();
-}
+} // namespace Client
--- FlightLoggingSoftware/Client/Main/Main.cpp
+++ FlightLoggingSoftware/Client/Main/Main.cpp
@@ -1,15 +1,27 @@
 #include "../../DataProtocol/DataProtocol.h"
+#include "../CsvReader/CsvReader.h"
 #include "../NetworkClient/NetworkClient.h"
 #include <boost/asio.hpp>
 #include <iostream>
 
+namespace Client {
+uint_fast64_t flight_id;
+uint_fast64_t initialTime;
+} // namespace Client
+
 int main(int argc, char *argv[]) {
   try {
     // First argument is IP or hostname
-    char* ip = argv[1];
+    char *ip = argv[1];
 
     // Second is flight ID
-    uint_fast64_t flight_id = atoi(argv[2]);
+    Client::flight_id = atoi(argv[2]);
+
+    // Initialize
+    Client::CsvReader input(
+        "/workspaces/CSCN73060-Group6/FlightLoggingSoftware/Client/"
+        "Telem_czba-cykf-pa28-w2_2023_3_1 12_31_27.txt");
+    Client::initialTime = 0;
 
     // Create an io_context object
     boost::asio::io_context io_context;
@@ -25,25 +37,20 @@ int main(int argc, char *argv[]) {
     // Connect to the server
     boost::asio::connect(socket, endpoints);
 
+    while (!input.isAtEnd()) {
+      DataProtocol::ClientTransmission *transmission =
+          input.getNextTransmission().release();
+      std::cout << transmission->getFlightId() << " "
+                << transmission->getSecondDelta() << " "
+                << transmission->getFuelLevel() << std::endl;
 
-    uint_fast64_t secondDelta = 0;
-    uint_fast64_t fuelLevel = 10000;
-
-    int numberOfTransmissions = 3000;
-
-    for (int i = 0; i < numberOfTransmissions; i++) {
-      DataProtocol::ClientTransmission dummy(flight_id, secondDelta, fuelLevel);
-      secondDelta++;
-      fuelLevel--;
-
-      boost::asio::write(
-          socket,
-          boost::asio::buffer(dummy.getPayload(), DataProtocol::PACKET_SIZE));
+      boost::asio::write(socket,
+                         boost::asio::buffer(transmission->getPayload(),
+                                             DataProtocol::PACKET_SIZE));
     }
 
-    DataProtocol::ClientTransmission endOfTransmission(flight_id, 0, fuelLevel);
-
-    boost::asio::write(socket, boost::asio::buffer(endOfTransmission.getPayload(),
+    DataProtocol::ClientTransmission transmission(Client::flight_id, 0, 0);
+    boost::asio::write(socket, boost::asio::buffer(transmission.getPayload(),
                                                    DataProtocol::PACKET_SIZE));
 
     // Close the socket
